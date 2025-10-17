439cb1542a600ffc13bd46f4de9b5de96a76aa64
dlpinheiro0012 <davipinheiro1266@gmail.com>
2025-10-16 22:59:59 -0300

    Update initial menu rendering and key handling
    Refactor initial menu drawing and navigation logic.

--- src/menu_inicial.c
+++ src/menu_inicial.c
@@ -1,13 +1,26 @@
-#include <stdio.h>
-#include "raylib.h"
-#include "menu_inicial.h"
+#include "menu.h"
 
-void init_menu(){
+TelaAtual AtualizarMenu(void) {
     BeginDrawing();
+        ClearBackground(BLACK);
 
-        ClearBackground(GRAY);
+        // Gerando o texto de título
+        const char *titulo = "Prepare-se para jogar!";
+        int fontSizeTitulo = 40;
+        int textWidthTitulo = MeasureText(titulo, fontSizeTitulo);
+        DrawText(titulo, (GetScreenWidth() - textWidthTitulo)/2, 150, fontSizeTitulo, RAYWHITE);
 
-        DrawText("TESTE MENU INICIAL", 250, 500, 40, GREEN);
+        // Gerando as opções de lógica (agr apenas pra créditos)
+        const char *op1 = "Press SPACE to Play";
+        const char *op2 = "Press C to Credits";
+        int fontSizeOp = 20;
+
+        DrawText(op1, (GetScreenWidth() - MeasureText(op1, fontSizeOp))/2, 250, fontSizeOp, GRAY);
+        DrawText(op2, (GetScreenWidth() - MeasureText(op2, fontSizeOp))/2, 280, fontSizeOp, GRAY);
 
     EndDrawing();
+
+    // Lógica de navegação
+    if (IsKeyPressed(KEY_C)) return TELA_CREDITOS;
+    else return TELA_MENU;
 }
