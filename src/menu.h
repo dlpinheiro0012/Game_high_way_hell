1737f36a1020ce3258934df24b04b49e8edbe78a
dlpinheiro0012 <davipinheiro1266@gmail.com>
2025-10-16 22:51:53 -0300

    Refactor menu.h to use TelaAtual enum
    Removed Menu structure and related functions, added TelaAtual enum and AtualizarMenu function.

--- src/menu.h
+++ src/menu.h
@@ -3,18 +3,12 @@
 
 #include "raylib.h"
 
-// Estrutura para armazenar o fundo do menu
-typedef struct {
-    Texture2D background;
-} Menu;
+typedef enum {
+    TELA_MENU,
+    TELA_CREDITOS
+} TelaAtual;
 
-// Inicializa o menu (carrega imagem de fundo)
-void InitMenu(Menu *menu);
-
-// Atualiza e desenha o menu (loop principal do menu)
-void DrawMenu(Menu *menu);
-
-// Libera recursos do menu (descarrega textura)
-void UnloadMenu(Menu *menu);
+// Função para desenhar o menu e retornar a próxima tela
+TelaAtual AtualizarMenu(void);
 
 #endif
