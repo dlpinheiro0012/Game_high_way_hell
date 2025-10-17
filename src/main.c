d2b8dbe2f02d370d538ed71303abfa3ddc12e1fc
dlpinheiro0012 <davipinheiro1266@gmail.com>
2025-10-16 23:02:56 -0300

    Refactor main function and improve readability
    Refactor main function and update screen dimensions.

--- src/main.c
+++ src/main.c
@@ -1,23 +1,28 @@
 #include "raylib.h"
-#include "load.h" //PARA FAZER LOAD DE ASSETS: USAR carregar_assets();
-#include "menu.h" //PARA INICIAR MENU: USAR init_menu();
-#include "creditos.h" //PARA INICIAR CRÉDITOS: USAR init_creditos();
+#include "menu.h"
+#include "creditos.h"
 
 int main(void) {
-    const int largura_tela = 1920;
-    const int altura_tela = 1080;
-    
-    InitWindow(largura_tela, altura_tela, "Highway to Hell");
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+    InitWindow(screenWidth, screenHeight, "High Way Hell");
     SetTargetFPS(60);
-    carregar_assets();
 
-    while (!WindowShouldClose())
-    {
-        init_menu();
-        //init_creditos();
+    TelaAtual tela = TELA_MENU;
+
+    while (!WindowShouldClose()) {
+        switch (tela) {
+            case TELA_MENU:
+                tela = AtualizarMenu();
+                break;
+
+            case TELA_CREDITOS:
+                AtualizarCreditos();
+                if (IsKeyPressed(KEY_ESCAPE)) tela = TELA_MENU;
+                break;
+        }
     }
 
     CloseWindow();
-
     return 0;
 }
