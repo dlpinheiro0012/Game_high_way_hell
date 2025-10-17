fe2c32b1d16019c4492d4af9f9efeda99a8e6412
dlpinheiro0012 <davipinheiro1266@gmail.com>
2025-10-16 22:41:52 -0300

    Refactor menu header and add menu functions
    Added menu structure and functions for initialization, drawing, and unloading.

--- src/menu_inicial.h
+++ src/menu_inicial.h
@@ -1,6 +1,20 @@
-#ifndef MENU_INICIAL_H
-#define MENU_INICIAL_H
+#ifndef MENU_H
+#define MENU_H
 
-void init_menu();
+#include "raylib.h"
+
+// Estrutura para armazenar o fundo do menu
+typedef struct {
+    Texture2D background;
+} Menu;
+
+// Inicializa o menu (carrega imagem de fundo)
+void InitMenu(Menu *menu);
+
+// Atualiza e desenha o menu (loop principal do menu)
+void DrawMenu(Menu *menu);
+
+// Libera recursos do menu (descarrega textura)
+void UnloadMenu(Menu *menu);
 
 #endif
