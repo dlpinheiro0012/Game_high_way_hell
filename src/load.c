6ca740086e8230d26993e292b6c117700083ca24
Davi José <davi.djsb@hotmail.com>
2025-11-17 19:46:14 -0300

    Delete src directory

--- src/load.c
+++
@@ -1,8 +0,0 @@
-#include "raylib.h"
-#include "load.h"
-#include <stdio.h>
-
-void carregar_assets(){
-    Image icon = LoadImage("assets/icon.png");
-    SetWindowIcon(icon);
-}
--- src/music_theme.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "raylib.h"
-#include "load_music.h"
-
-void init_music() {
-    Music musicTheme = LoadMusicStream("assets\music_HWH.mp3");
-    if (IsMusicValid(musicTheme)) PlayMusicStream(musicTheme);
-}
