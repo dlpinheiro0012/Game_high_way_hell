3b1d2c89459df147020052589148be0782266671
Matt-747 <matheussv.0704@gmail.com>
2025-12-10 18:35:20 -0300

    Novos_Textos

--- source/interludio.c
+++ source/interludio.c
@@ -5,14 +5,13 @@ void InitInterludio(Screen* screenSelector) {
     BeginDrawing();
     ClearBackground(BLACK);
 
-    DrawTextDigitado("A luta foi árdua, mas você conseguiu derrotar Googlios com todas as suas forças. Em seu leito de morta, o demônio disse:", larguraTela/15, alturaTela/7, 45, WHITE,1);
-    DrawTextDigitado("'Eu já cumpri meu papel... Você nunca derrotará Jobos, mortal! Renda-se e preze pela sua vida!'", larguraTela/15, alturaTela/5.5, 45, WHITE,0.6);
-
-    DrawTextDigitado("'Então, diante de você, uma fenda dimensional surge, onde se revela o outro lado - o Inferno da Maçã -'", larguraTela/15, alturaTela/4, 45, WHITE,0.6);
-    DrawTextDigitado("e no horizonte flamejante e vermelho, o ICastelo de Jobos, onde Alexandrina ainda é mantida presa.", larguraTela/15, alturaTela/3.5, 45, WHITE,0.6);
-
-    DrawTextDigitado("", larguraTela/15, alturaTela/2.8, 45, WHITE,0.6);
-    DrawTextDigitado("impenetrável... Vença-a e siga HIGH WAY TO HELL!!!", larguraTela/15, alturaTela/2.5, 45, WHITE,0.6);
+    DrawTextDigitado("A luta foi árdua, mas você conseguiu derrotar Googlios.", larguraTela/15, alturaTela/7, 35, WHITE,1);
+    DrawTextDigitado("Em seu leito de morte, o demônio disse:", larguraTela/15, alturaTela/5, 35, WHITE,1);
+    DrawTextDigitado("'Eu já cumpri meu papel... Você nunca derrotará Jobos, mortal!", larguraTela/15, alturaTela/4, 35, YELLOW,0.6);
+    DrawTextDigitado("Renda-se se preza pela sua vida!'", larguraTela/15, alturaTela/3.4, 35, YELLOW,0.6);
+    DrawTextDigitado("Então, diante de você se revela o outro lado - o ICastle, o castelo do infame Jobos,", larguraTela/15, alturaTela/2.9, 35, WHITE,0.6);
+    DrawTextDigitado("que ainda mantém presa sua amada Alexandrina.", larguraTela/15, alturaTela/2.5, 35, WHITE,0.6);
+    DrawTextDigitado("Nesse momento, você entra no portal, pronto para a batalha final.", larguraTela/15, alturaTela/2.2, 35, WHITE,0.6);
 
     DrawTextDigitado("Pressione D para atingir seu inimigo", larguraTela/15, 1.5 * alturaTela / 2, 30, RED,0.6);
     DrawTextDigitado("Pressione H para começar", larguraTela/15, 1.5 * alturaTela / 1.9, 30, RED,0.6);
